a5c41d2a6f39e4607ed06850d94c254c44f7550e
Chloé VIEILLE <37896835+ChloeViei@users.noreply.github.com>
2018-05-17 10:01:53 +0200

    Update Bonus.java

--- Bonus.java
+++ Bonus.java
@@ -6,6 +6,7 @@ public class Bonus {
     public Point p;
     private static final int longueur = 50;     // represente les dimensions de la fenetre
     private static final int largeur = 70;     // pour eviter que les pommes soient positionner a l'extérieur
+    public int typeBonus;
 
     public Bonus(){
         Random r = new Random();            // on determine aleatoirement la position de la pomme
@@ -15,5 +16,18 @@ public class Bonus {
         y = y * 10 + 10;                    // +10 pour éviter que la pomme se colle au bord se qui rend difficile de la récuperer
 
         p = new Point(x,y);
+
+        typeBonus();
+    }
+
+
+    public void typeBonus(){
+
+        Random r = new Random();
+        int valeur = 1 + r.nextInt(4 - 1);
+
+        System.out.println("Valeur bonus" + valeur);
+
+        this.typeBonus = valeur;
     }
 }
