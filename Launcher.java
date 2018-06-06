9011f428e1e8ec3caa62f22b4df8509d600ceb70
Chloé VIEILLE <37896835+ChloeViei@users.noreply.github.com>
2018-06-06 19:05:34 +0200

    Update Launcher.java

--- Launcher.java
+++ Launcher.java
@@ -1,43 +1,58 @@
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URL;
 
 public class Launcher extends JFrame {
 
-    private JLabel pseudoLabel, titre, pseudo2Label, titreHelp;
-    private JFrame frame3;
-    private JPanel panoTitre, panoInfo, panoInfo2, panoInfo3, panoBouton, panoGlobal, panoCentral;
-    private JButton start, help, multijoueur, unJoueur, meilleurScore;
-    private JTextField pseudoText, pseudo2Text;
+    private JFrame parent = new JFrame();
+    private Icon icon;
+    private URL image = getClass().getResource("image/winner.png");
+    private MeilleurScore meilleurScore = new MeilleurScore();
+
+    private JPanel jPanel, jPanel1, jPanel2, jPanel3, jPanel4, jPanel5, jPanel6, jPanel7,
+            jPanel8 ;
+    private JLabel jLabel1, jLabel2, jLabel3, jLabel4 ;
+    private JTextField jTextField1, jTextField2, jTextField3;
+    private JButton jButton1, jButton2, jButton3, jButton4;
     private JOptionPane erreur;
     private JDialog erreurDialog;
-    private JPanel centreTitre, centreTitreHelp;
+
+    private JLabel titreHelp;
+    private JPanel panoTitre, panoBouton, panoGlobal;
+    private JButton unJoueur;
+    private JPanel centreTitre;
     private boolean multi = false, unPlayer = true, verification;
 
     public Launcher() {
-        creerWidget();
+        initComponent();
         afficheWidget();
         action();
-        setSize(500,400);
+        setSize(400,250);
+        setLocation(400,200);
         setVisible(true);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
     private void launcherSolo() {
-        creerWidget();
+        initComponent();
         afficheWidget();
         action();
-        setSize(500,400);
+        setSize(400,250);
         setVisible(true);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
     private void launcherMulti() {
-        creerWidget();
+        initComponent();
         afficheWidgetMulti();
         action();
-        setSize(500,400);
+        setSize(400,300);
         setVisible(true);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
@@ -46,40 +61,53 @@ public class Launcher extends JFrame {
         creerWidgetHelp();
         afficheWidgetHelp();
         action();
-        setSize(500,400);
+        setSize(400,300);
         setVisible(true);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
     private void action() {
-        start.addActionListener(new ActionListener() {
+        jButton1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e){
                 verification = true;
-                if (multi && (pseudoText.getText().isEmpty() || pseudo2Text.getText().isEmpty()) || (!multi && pseudoText.getText().isEmpty())){
+                if (multi && (jTextField2.getText().isEmpty() || jTextField3.getText().isEmpty()) || (!multi && jTextField1.getText().isEmpty())){
                     verification = false;
-                    erreur.showMessageDialog(start, "Veuillez entrer un pseudo", "Erreur",JOptionPane.ERROR_MESSAGE);
-                    erreurDialog = erreur.createDialog(start, "Erreur");
+                    erreur.showMessageDialog(jButton1, "Veuillez entrer un pseudo", "Erreur",JOptionPane.ERROR_MESSAGE);
+                    erreurDialog = erreur.createDialog(jButton1, "Erreur");
                 }
                 if(unPlayer && verification){
-                    Snake frame = new Snake(1, pseudoText.getText(), "");
+                    Snake frame = new Snake(1, jTextField1.getText(), "");
                     frame.pack();
                     frame.setVisible(true);
                 }
                 if (multi && verification){
-                    Snake frame = new Snake(2, pseudoText.getText(), pseudo2Text.getText());
+                    Snake frame = new Snake(2, jTextField2.getText(), jTextField3.getText());
                     frame.pack();
                     frame.setVisible(true);
                 }
             }
         });
 
-        help.addActionListener(new ActionListener() {
+
+        jButton2.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e){
-                help();
+                String[] options = {"OK"};
+                JOptionPane jOptionPane = new JOptionPane();
+
+                String messageMeilleurScore = "Meilleurs Scores : \n\n Premier de " + meilleurScore.getMeilleurPseudo().get(0) + " :    " + meilleurScore.getMeilleurScore().get(0) + " \n Deuxième de " + meilleurScore.getMeilleurPseudo().get(1) + " :   " + meilleurScore.getMeilleurScore().get(1) + " \n Troisième de " + meilleurScore.getMeilleurPseudo().get(2) + " :    " + meilleurScore.getMeilleurScore().get(2) + "\n";
+                int x = jOptionPane.showOptionDialog(parent, messageMeilleurScore + "\n", "Meilleur Score",JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, icon, options, options[0]);
+
+                if (x == 0 ){
+                    if (multi){
+                        launcherMulti();
+                    }else {
+                        launcherSolo();
+                    }
+                }
             }
         });
 
-        multijoueur.addActionListener(new ActionListener() {
+        jButton3.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 multi = true;
                 unPlayer = false;
@@ -87,7 +115,7 @@ public class Launcher extends JFrame {
             }
         });
 
-        unJoueur.addActionListener(new ActionListener() {
+        jButton4.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 unPlayer = true;
                 multi = false;
@@ -96,37 +124,117 @@ public class Launcher extends JFrame {
         });
     }
 
-    private void creerWidget() {
+    private void initComponent() {
 
-        titre = new JLabel("SNAKE");
-        centreTitre = new JPanel();
-        centreTitre.add(titre);
-        titre.setFont(new Font(titre.getFont().getName(), titre.getFont().getStyle(), 30));
-        pseudoLabel = new JLabel("Joueur 1");
-        pseudo2Label = new JLabel("Joueur 2");
+        icon = new ImageIcon(image);
 
-        start = new JButton("Jouer");
-        help = new JButton("Aide");
-        multijoueur = new JButton("Multijoueur");
-        unJoueur = new JButton("Retour");
+        jPanel = new JPanel(new BorderLayout());
 
-        pseudoText = new JTextField();
-        pseudoText.setColumns(10);
-        pseudo2Text = new JTextField();
-        pseudo2Text.setColumns(10);
+        jPanel1 = new JPanel();
+        jPanel1.setBackground(new Color(36, 47, 65));
+        jPanel2 = new JPanel();
+        jPanel2.setBackground(new Color(36, 47, 65));
+        jPanel3 = new JPanel();
+        jPanel3.setBackground(new Color(36, 47, 65));
+        jPanel4 = new JPanel();
+        jPanel4.setBackground(new Color(36, 47, 65));
+        jPanel5 = new JPanel();
+        jPanel5.setBackground(new Color(36, 47, 65));
+        jPanel6 = new JPanel(new BorderLayout());
+        jPanel6.setBackground(new Color(36, 47, 65));
 
-        panoTitre = new JPanel(new BorderLayout());
-        panoInfo = new JPanel();
-        panoInfo2 = new JPanel();
-        panoInfo3 = new JPanel();
-        panoBouton = new JPanel();
-        panoCentral = new JPanel();
-        panoGlobal = new JPanel();
+        jLabel1 = new JLabel();
+        jLabel1.setFont(new Font("Century Gothic", 0, 35));
+        jLabel1.setForeground(new Color(212, 212, 212));
+        jLabel1.setText("             SNAKE             ");
+        jLabel1.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, new Color(255, 255, 255)));
+
+        jLabel2 = new JLabel();
+        jLabel2.setFont(new Font("Century Gothic", 0, 20));
+        jLabel2.setForeground(new Color(255,255,255));
+        jLabel2.setText("Pseudo : ");
+
+        jTextField1 = new JTextField();
+        jTextField1.setColumns(10);
+        jTextField1.setBackground(new Color(36,47,65));
+        jTextField1.setFont(new Font("Century Gothic", 0, 13));
+        jTextField1.setForeground(new Color(255,255,255));
+        jTextField1.setText("");
+        jTextField1.setOpaque(false);
+        jTextField1.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, new Color(166, 166, 166)));
+
+
+        jLabel3 = new JLabel();
+        jLabel3.setFont(new Font("Century Gothic", 0, 20));
+        jLabel3.setForeground(new Color(255,255,255));
+        jLabel3.setText("Pseudo Joueur 1 : ");
+
+        jTextField3 = new JTextField();
+        jTextField3.setColumns(10);
+        jTextField3.setBackground(new Color(36,47,65));
+        jTextField3.setFont(new Font("Century Gothic", 0, 13));
+        jTextField3.setForeground(new Color(255,255,255));
+        jTextField3.setText("");
+        jTextField3.setOpaque(false);
+        jTextField3.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, new Color(166, 166, 166)));
+
+
+        jLabel4 = new JLabel();
+        jLabel4.setFont(new Font("Century Gothic", 0, 20));
+        jLabel4.setForeground(new Color(255,255,255));
+        jLabel4.setText("Pseudo Joueur 2 : ");
+
+        jTextField2 = new JTextField();
+        jTextField2.setColumns(10);
+        jTextField2.setBackground(new Color(36,47,65));
+        jTextField2.setFont(new Font("Century Gothic", 0, 13));
+        jTextField2.setForeground(new Color(255,255,255));
+        jTextField2.setText("");
+        jTextField2.setOpaque(false);
+        jTextField2.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, new Color(166, 166, 166)));
+
+
+        jButton1 = new JButton();
+        jButton1.setText("Jouer");
+        jButton1.setFont(new Font("Century Gothic", 0, 22));
+        jButton1.setForeground(new Color(191, 186, 186));
+        jButton1.setBackground(new Color(105, 50, 104));
+        Border line = new LineBorder(Color.BLACK);
+        Border margin = new EmptyBorder(5, 15, 5, 15);
+        Border compound = new CompoundBorder(line, margin);
+        jButton1.setBorder(compound);
+        jButton1.setFocusPainted(false);
+
+        jButton2 = new JButton();
+        jButton2.setText("Info");
+        jButton2.setFont(new Font("Century Gothic", 0, 13));
+        jButton2.setForeground(new Color(191, 186, 186));
+        jButton2.setBackground(new Color(92, 44, 91));
+        jButton2.setBorder(compound);
+        jButton2.setFocusPainted(false);
+
+        jButton3 = new JButton();
+        jButton3.setText("Multi");
+        jButton3.setFont(new Font("Century Gothic", 0, 13));
+        jButton3.setForeground(new Color(191, 186, 186));
+        jButton3.setBackground(new Color(92, 44, 91));
+        jButton3.setBorder(compound);
+        jButton3.setFocusPainted(false);
+
+        jButton4 = new JButton();
+        jButton4.setText("Solo ");
+        jButton4.setFont(new Font("Century Gothic", 0, 13));
+        jButton4.setForeground(new Color(191, 186, 186));
+        jButton4.setBackground(new Color(92, 44, 91));
+        jButton4.setBorder(compound);
+        jButton4.setFocusPainted(false);
+
 
         erreur = new JOptionPane();
     }
 
     private void creerWidgetHelp(){
+
         titreHelp = new JLabel("Aide");
         centreTitre = new JPanel();
         centreTitre.add(titreHelp);
@@ -147,51 +255,50 @@ public class Launcher extends JFrame {
         panoGlobal.add(panoTitre);
         panoGlobal.add(panoBouton);
         setContentPane(panoGlobal);
+
     }
 
     private void afficheWidget(){
 
-        panoTitre.add(centreTitre, BorderLayout.NORTH);
-        panoInfo.add(pseudoLabel);
-        panoInfo.add(pseudoText);
-        panoCentral.setLayout(new BoxLayout(panoCentral, BoxLayout.Y_AXIS));
-        panoCentral.add(panoInfo);
-        panoCentral.add(panoInfo3);
-        panoTitre.add(panoCentral, BorderLayout.CENTER);
+        jPanel1.add(jLabel1);
+        jPanel2.add(jLabel2);
+        jPanel2.add(jTextField1);
+        jPanel3.add(jButton2);
+        jPanel3.add(jButton1);
+        jPanel3.add(jButton3);
 
-        panoBouton.add(multijoueur);
-        panoBouton.add(start);
-        panoBouton.add(help);
-        panoGlobal.setLayout(new BoxLayout(panoGlobal, BoxLayout.Y_AXIS));
-        panoGlobal.add(panoTitre);
-        panoGlobal.add(panoBouton);
+        jPanel.add(jPanel1, BorderLayout.NORTH);
+        jPanel.add(jPanel2, BorderLayout.CENTER);
+        jPanel.add(jPanel3, BorderLayout.SOUTH);
 
-        setContentPane(panoGlobal);
+        setContentPane(jPanel);
+
     }
 
     private void afficheWidgetMulti(){
 
-        panoTitre.add(centreTitre, BorderLayout.NORTH);
-        panoInfo.add(pseudoLabel);
-        panoInfo2.add(pseudo2Label);
-        panoInfo.add(pseudoText);
-        panoInfo2.add(pseudo2Text);
 
-        panoCentral.setLayout(new BoxLayout(panoCentral, BoxLayout.Y_AXIS));
-        panoCentral.add(panoInfo);
-        panoCentral.add(panoInfo2);
-        panoCentral.add(panoInfo3);
-        panoTitre.add(panoCentral, BorderLayout.CENTER);
+        jPanel1.add(jLabel1);
+        jPanel2.add(jLabel3);
+        jPanel2.add(jTextField2);
+        jPanel4.add(jLabel4);
+        jPanel4.add(jTextField3);
+        jPanel3.add(jButton2);
+        jPanel3.add(jButton1);
+        jPanel3.add(jButton4);
 
-        panoBouton.add(unJoueur);
-        panoBouton.add(start);
-        panoBouton.add(help);
+        jPanel5.setLayout(new BoxLayout(jPanel5, BoxLayout.Y_AXIS));
 
-        panoGlobal.setLayout(new BoxLayout(panoGlobal, BoxLayout.Y_AXIS));
-        panoGlobal.add(panoTitre);
-        panoGlobal.add(panoBouton);
+        jPanel5.add(jPanel2);
+        jPanel5.add(jPanel4);
 
-        setContentPane(panoGlobal);
+        jPanel.add(jPanel1, BorderLayout.NORTH);
+        jPanel.add(jPanel5, BorderLayout.CENTER);
+        jPanel.add(jPanel3, BorderLayout.SOUTH);
+
+
+        setContentPane(jPanel);
+
     }
 
     public static void main(String[] args) {
